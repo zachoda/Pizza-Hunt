ea4ac262f9fb1f2598fc3448b3f5d5b8a9ac0990
Zach Oda <zjodachow@gmail.com>
2022-09-28 17:40:35 -0400

    updated model

---
+++ models/Pizza.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose');
+
+const PizzaSchema = new Schema({
+    pizzaName: {
+        type: String
+    },
+    createdBy: {
+        type: String
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    size: {
+        type: String,
+        default: 'Large'
+    },
+    toppings: []
+
+});
+
+//Create the Pizza model using the PizzaScheme
+const Pizza = model('Pizza', PizzaSchema);
+
+//export the pizza model
+module.exports = Pizza;
---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controllers');
+
+//set up GET all and POST at /api/pizzas
+router
+.route('/')
+.get(getAllPizza)
+.post(createPizza);
+
+//set up Get one, Put & Delete at /api/pizzas/:id
+router
+.route('/:id')
+.get(getPizzaById)
+.put(updatePizza)
+.delete(deletePizza);
+
+module.exports = router;
